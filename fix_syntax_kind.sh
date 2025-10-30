66684e641f09e2412fdda2456926edefec281d21
Yoandy Rodriguez <yoandy.rmartinez@gmail.com>
2025-10-30 21:35:09 +0200

    fix: we don't need this anymore

--- fix_syntax_kind.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Fix all instances of SyntaxKind.into() in parser/mod.rs
-sed -i 's/SyntaxKind::\([a-zA-Z]*\)\.into()/CitrineLanguage::kind_to_raw(SyntaxKind::\1)/g' src/parser/mod.rs
-
-# Fix the token method call
-sed -i 's/self\.builder\.token(kind\.into(), token\.text\.as_str());/self.builder.token(CitrineLanguage::kind_to_raw(kind), token.text.as_str());/g' src/parser/mod.rs
-
